f1442fe73577db4708dcd16e337dbb716adcabaa
ashish.stit <ashish.stit@outlook.com>
2020-08-31 12:27:45 +0530

    'second'

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -6,6 +6,7 @@ use App\User;
 use Illuminate\Support\Facades\Auth; 
 use App\Http\Requests\ResetValidation;
 use Validator;
+use File;
 use Twilio\Rest\Client;
 use Twilio\Jwt\ClientToken;
 use Illuminate\Support\Facades\Session;
@@ -18,25 +19,54 @@ public $successStatus = 200;
      * 
      * @return \Illuminate\Http\Response 
      */ 
-    public function login(){
-     $user = User::find(Auth::id());
+
+
+    private function respondWithError($code, $message, $data)
+  {
+    return response()->json(array('code'=>$code,'message'=>$message,'data'=>$data));
+  }
+
+     public function login(Request $request){
+
+                 if(is_numeric($request->get('email')))
+         {
+
+             $user = User::find(Auth::id());
+             if(Auth::attempt(['mobile_no' => "+91".''.$request->email, 'password' => $request->password])){ 
+            $user = Auth::user(); 
+            return response()->json(['result'=>'Login Successful','user_id'=>$user->id,'name'=>$user->name,'status'=>'1','token'=>$user->createToken('MyApp')-> accessToken], $this-> successStatus); 
+        } 
+        else{ 
+             return response()->json(['error'=>'Invalid Username and Password!','status'=>'0'], 401); 
+             
+            } 
+          }
+          elseif (filter_var($request->get('email'), FILTER_VALIDATE_EMAIL)) {
+            $user = User::find(Auth::id());
     
         if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){ 
             $user = Auth::user(); 
-            return response()->json(['result'=>'Login Successful','name'=>$user->name,'status'=>'1','token'=>$user->createToken('MyApp')-> accessToken], $this-> successStatus); 
+            return response()->json(['result'=>'Login Successful','name'=>$user->name,'user_id'=>$user->id,'status'=>'1','token'=>$user->createToken('MyApp')-> accessToken], $this-> successStatus); 
         } 
         else{ 
              return response()->json(['error'=>'Invalid Username and Password!','status'=>'0'], 401); 
              
 
         } 
+          }
+
+
+
+
+
+
     }
 /** 
      * Register api 
      * 
      * @return \Illuminate\Http\Response 
      */ 
-    public function register(Request $request) 
+    public function registertwilio(Request $request) 
     { 
         
         $validator = Validator::make($request->all(), [ 
@@ -78,13 +108,14 @@ public $successStatus = 200;
             echo "Something wrong";
         }
     }
-    public function verifyOtp(Request $request)
+    public function verifyOtptwilio(Request $request)
     {
         try
         {
            $otp=$request->Session()->get('otp');
            $user_name=$request->Session()->get('user_name');
            $email=$request->Session()->get('email');
+
            $password=$request->Session()->get('password');
            $mobile_no=$request->Session()->get('mobile_no');
            $city=$request->Session()->get('city');
@@ -98,7 +129,7 @@ public $successStatus = 200;
          $user->email=$email;
          $user->name=$user_name;
          $user->password=$password;
-         $user->mobile_no=$mobile_no;
+         $user->mobile_no='+91'.''.$mobile_no;
          $user->city=$city;
          $user->sex=$sex;
          $user->dob=$dob;
@@ -140,7 +171,7 @@ public $successStatus = 200;
 
             if($user_data->save())
             {           
-          return response()->json(['result'=>'Login Successful','id'=>$user_data->id,'name'=>$name,'status'=>'1','token'=>$user_data->createToken('MyApp')-> accessToken], $this-> successStatus); 
+          return response()->json(['id'=>$user_data->id,'status'=>'2'], $this-> successStatus); 
 
 
             } 
@@ -152,13 +183,13 @@ public $successStatus = 200;
              
         } 
         else{ 
-          return response()->json(['result'=>'Login Successful','name'=>$user->name,'status'=>'1','token'=>$user->createToken('MyApp')-> accessToken], $this-> successStatus); 
+          return response()->json(['result'=>'Login Successful','user_id'=>$user->id,'name'=>$user->name,'status'=>'1','token'=>$user->createToken('MyApp')-> accessToken], $this-> successStatus); 
             
               } 
     
 
     }
-    public function facebookprofl(Request $request)
+ public function facebookprofl(Request $request)
     {
       $id=$request->id;
       $sex=$request->sex;
@@ -170,11 +201,130 @@ public $successStatus = 200;
       $profile_data->dob=$dob;
       if($profile_data->save())
       {
-       return response()->json(['result'=>'Profile Updated','status'=>'1'], $this-> successStatus); 
+       return response()->json(['result'=>'Login Successful','status'=>'1','user_id'=>$profile_data->id,'name'=>$profile_data->name,'token'=>$profile_data->createToken('MyApp')-> accessToken], $this-> successStatus); 
       }
       else
             {
              return response()->json(['error'=>'Unauthorised','status'=>'0'], 401); 
             }
     }
+ public function register(Request $request) 
+    { 
+        
+        $validator = Validator::make($request->all(), [  
+            'password' => 'required', 
+            'c_password' => 'required|same:password', 
+        ]);
+     if ($validator->fails()) { 
+            return response()->json(['error'=>$validator->errors()], 401);            
+        }
+        try
+        {
+        $email=$request->email;
+        $password=$request->password;
+        $mobile_no='+91'.''.$request->mobile_no;
+        $user_name=$request->username;
+        $city=$request->city;
+        $sex=$request->sex;
+        $country=$request->country;
+        $state=$request->state;
+        $district=$request->district;
+        $dob=$request->dob;
+        $user_data=User::where('mobile_no', $mobile_no)->orWhere('email', $email)->first();
+        if($user_data == "")
+          {
+           return response()->json(['status'=>'1','email'=>$email,'password'=>$password,'mobile_no'=>$request->mobile_no,'username'=>$user_name,'city'=>$city,'sex'=>$sex,'dob'=>$dob,'country'=>$country,'state'=>$state,'district'=>$district], $this-> successStatus); 
+        
+            }
+       else 
+       {
+        return response()->json(['result'=>'Mobile number or email already exist!','status'=>'0'], 401);
+          }
+       
+        
+    }
+     catch (Exception $e)
+        {
+            echo "Something wrong";
+        }
+    }
+public function verified(Request $request) 
+    { 
+        
+        $validator = Validator::make($request->all(), [ 
+            'email' => 'required|email|unique:users', 
+            'password' => 'required', 
+            'c_password' => 'required|same:password', 
+        ]);
+     if ($validator->fails()) { 
+            return response()->json(['error'=>$validator->errors()], 401);            
+        }
+        try
+        {
+        $email=$request->email;
+        $password=$request->password;
+        $mobile_no='+91'.''.$request->mobile_no;
+        $user_name=$request->username;
+        $city=$request->city;
+        $sex=$request->sex;
+        $country=$request->country;
+        $state=$request->state;
+        $district=$request->district;
+        $dob=$request->dob;
+         $user=new User;
+         $user->email=$email;
+         $user->name=$user_name;
+         $user->password=bcrypt($password);
+         $user->mobile_no=$mobile_no;
+         $user->country=$country;
+         $user->state=$state;
+         $user->district=$district; 
+         $user->city=$city;
+         $user->sex=$sex;
+         $user->dob=$dob;
+         if($user->save())
+        {
+           return response()->json(['result'=>'Register Successful','user_id'=>$user->id,'name'=>$user->name,'status'=>'1','token'=>$user->createToken('MyApp')-> accessToken], $this-> successStatus); 
+        } 
+        else{ 
+            return response()->json(['result'=>'Unauthorised','status'=>'0'], 401);        }       
+        
+    }
+     catch (Exception $e)
+        {
+            echo "Something wrong";
+        }
+    }
+
+public function userprofile(Request $request)
+{
+  try{
+    $user_id=$request->user_id;
+    $file = $request->file('profile');
+      if (!$file->isValid()) {
+        return $this->respondWithError(400,"Invalid Content",array());
+      }
+  $uploadPath=public_path()."/userimage";
+  $ext = explode('.',$file->getClientOriginalName());
+   $extenstions = array(
+        'image'=>array('jpg','jpeg','png'));  
+   if (!in_array($ext[1], $extenstions['image'])) 
+      {
+        return response()->json(['result'=>'Not an image file!','status'=>'0'], 401);      }
+
+      $filename = uniqid('media_').'_'.date('Y-m-d-h-i-s');
+      $filename = $filename.'.'.$ext[1];
+      $data = $file->move(trim($uploadPath), $filename);
+      $userimg =User::where('id', $user_id)->first();;
+      $userimg->media_path ="http://ec2-3-15-216-225.us-east-2.compute.amazonaws.com/public/userimage/".$filename;
+      $userimg->save();
+     return response()->json(['result'=>'Profile Updated successfully!','user_id'=>$userimg->id,'media_path'=>$userimg->media_path,'status'=>'1'], 401);}
+catch (\Exception $e) {
+      return $this->respondWithError(500,"Internal Server Error!",array());
+    }
+
+}
+
+
+
 }
